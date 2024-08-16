806f270bd281158c1e2f739ae18eb92a6ad7a458
kefy <kefy@mail.ru>
2024-08-16 09:44:17 +0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -2,10 +2,11 @@
 
 # Устанавливаем Zsh  
 sudo apt update  
+sudo apt install git -y
 sudo apt install zsh -y
 
 # Устанавливаем oh-my-zsh  
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Устанавливаем тему Powerlevel10k  
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
@@ -17,7 +18,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/z
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Настройка конфигурации Zsh  
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc  
+sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc  
 echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
 
 # Устанавливаем шрифт для отображения иконок Powerline  
