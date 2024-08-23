4c930ec2e2eee2e4236dd92514919c7cb4fb6483
kefy <kefy@mail.ru>
2024-08-23 12:56:26 +0300

    Create ohmyzsh.sh

---
+++ ohmyzsh.sh
@@ -0,0 +1,15 @@
+# Устанавливаем oh-my-zsh  
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Устанавливаем тему Powerlevel10k  
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Устанавливаем плагин zsh-autosuggestions  
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+# Устанавливаем плагин zsh-syntax-highlighting  
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Настройка конфигурации Zsh  
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc  
+echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
